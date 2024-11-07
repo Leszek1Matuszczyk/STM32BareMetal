cfdbc0ab8de43219f0437a2d23807087fdb7b194
Leszek1Matuszczyk <149710969+Leszek1Matuszczyk@users.noreply.github.com>
2024-11-07 11:06:04 +0100

    writing program switchin  PA5 LED on

--- 0_led_toggle_adr/Src/main.c
+++ 0_led_toggle_adr/Src/main.c
@@ -2,8 +2,8 @@
 //port : a
 //pin : 5
 
-#define PERIPH_BASE 			(0x4000 0000UL)
-#define AHB1PERIPH_OFFSET0 		(0x0002 0000UL)
+#define PERIPH_BASE 			(0x40000000UL)
+#define AHB1PERIPH_OFFSET 		(0x00020000UL)
 #define AHB1PERIPH_BASE			(PERIPH_BASE + AHB1PERIPH_OFFSET)
 #define GPIOA_OFFSET 			(0x0000U)
 
@@ -16,15 +16,39 @@
 
 
 #define AHB1EN_R_OFFSET			(0x30)
-#define RCC_AHB1_R				(*(volatile unsigned int)(RCC_BASE + AHB1EN_R_OFFSET))
+#define RCC_AHB1_R				(*(volatile unsigned int *)(RCC_BASE + AHB1EN_R_OFFSET))
 
 #define MODE_R_OFFSET			(0x00UL)
-#define GPIOA_MODE_R			(*(volatile unsigned int)(GPIOA_BASE + MODE_R_OFFSET))
+#define GPIOA_MODE_R			(*(volatile unsigned int *)(GPIOA_BASE + MODE_R_OFFSET))
 
 #define OD_R_OFFSET				(0x14UL)
-#define GPIOA_OD_R				(*(volatile unsigned int)(GPIOA_BASE + OD_R_OFFSET))
+#define GPIOA_OD_R				(*(volatile unsigned int *)(GPIOA_BASE + OD_R_OFFSET))
 
 #define GPIOAEN					(1U<<0)
 
 #define PIN5					(1U<<5)
 #define LED_PIN					PIN5
+
+//
+//(1U<<10) set bit 10 to 1
+//&=~(1U<<11) set bit 11 to 0
+//
+int main (void)
+{
+
+	//enable clock accest o GPIOA
+	RCC_AHB1_R |= GPIOAEN;
+	//SET PA5 as OUTPUT pin
+	GPIOA_MODE_R |= (1U<<10);
+	GPIOA_MODE_R &=~(1U<<11);
+
+	while(1)
+	{
+
+		//TOGGLE GPIOA PIN5
+		GPIOA_OD_R |= LED_PIN;
+	}
+
+}
+
+
